fe40cd0f32c1f53115abc6c919753972ad47b821
E. Hannah <echannah631@gmail.com>
2024-02-27 19:17:34 -0600

    added form validation using react hook form

--- src/ContactPage.tsx
+++ src/ContactPage.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { useForm, FieldError } from 'react-hook-form';
+import { ValidationError } from './ValidationError';
 
 type Contact = {
   name: string;
@@ -9,7 +10,15 @@ type Contact = {
 };
 
 export const ContactPage = () => {
-  const { register, handleSubmit } = useForm<Contact>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Contact>({
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+  });
+
   const navigate = useNavigate();
   const fieldStyle = 'flex flex-col mb-2';
 
@@ -18,44 +27,61 @@ export const ContactPage = () => {
     navigate(`/thank-you/${contact.name}`);
   };
 
+  const getEditorStyle = (fieldError: FieldError | undefined) => {
+    return fieldError ? 'border-red-500' : '';
+  };
+
   return (
     <div className='flex flex-col py-10 max-w-md mx-auto'>
       <h2 className='text-3xl font-bold underline mb-3'>Contact Us</h2>
       <p className='mb-3'>
         If you enter your details, we'll get back to you as soon as we can.
       </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className={fieldStyle}>
           <label htmlFor='name'>Your name</label>
           <input
             type='text'
             id='name'
-            {...register('name')}
-            required
+            {...register('name', { required: 'You must enter your name' })}
+            className={getEditorStyle(errors.name)}
           />
+          <ValidationError fieldError={errors.name} />
         </div>
         <div className={fieldStyle}>
           <label htmlFor='email'>Your email address</label>
           <input
             type='email'
             id='email'
-            {...register('email')}
-            required
-            pattern='\S+@\S+\.\S+'
+            {...register('email', {
+              required: 'You must enter your email address',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: 'Entered value does not match email format',
+              },
+            })}
+            className={getEditorStyle(errors.email)}
           />
+          <ValidationError fieldError={errors.email} />
         </div>
         <div className={fieldStyle}>
           <label htmlFor='reason'>Reason you need to contact us</label>
           <select
-            {...register('reason')}
+            {...register('reason', {
+              required: 'You must enter the reason for contacting us',
+            })}
             id='reason'
-            required
+            className={getEditorStyle(errors.reason)}
           >
             <option value=''></option>
             <option value='Support'>Support</option>
             <option value='Feedback'>Feedback</option>
             <option value='Other'>Other</option>
           </select>
+          <ValidationError fieldError={errors.reason} />
         </div>
         <div className={fieldStyle}>
           <label htmlFor='notes'>Additional notes</label>
